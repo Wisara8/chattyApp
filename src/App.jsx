262a16fd5826f977ffa37f66e3875439c3693e1b
Wisara8 <srandall845@gmail.com>
2018-07-10 20:26:09 +0000

    boiler plate setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,17 @@
 import React, {Component} from 'react';
+import ChatBar from './ChatBar.jsx';
+import Navbar from './components/navbar.jsx';
+import Message from './Message.jsx';
+
 
 class App extends Component {
   render() {
     return (
-      <h1>Hello React :)</h1>
+      <div>
+        <Navbar />
+        <Message />
+        <ChatBar />
+      </div>
     );
   }
 }
