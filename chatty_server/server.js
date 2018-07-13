2f9d76d067d6a8e71dae1959c6a6a96a2062f704
Wisara8 <srandall845@gmail.com>
2018-07-13 19:36:52 +0000

    fixed user count to update on client disconnect

--- chatty_server/server.js
+++ chatty_server/server.js
@@ -49,5 +49,12 @@ wss.on('connection', (ws) => {
 
   });  
   // Set up a callback for when a client closes the socket. This usually means they closed their browser.
-  ws.on('close', () => console.log('Client disconnected'));
+  ws.on('close', () => {
+    let counter = wss.clients.size;
+    wss.clients.forEach(function each(client) {
+      client.send(counter);
+    });
+    console.log('Client disconnected')
+  });
+  
 });
